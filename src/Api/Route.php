1da1ada7b29e541afba56062dd25878141c860b1
Jeroen de Graaf <hello@jero.work>
2020-12-07 18:28:16 +0100

    Setup Route attribute object
    This route attribute object will be the attribute to define routes in a
    controller/action.

---
+++ src/Api/Route.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jerowork\RouteAttributeProvider\Api;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
+final class Route
+{
+    private string $pattern;
+
+    /**
+     * @var string[]
+     */
+    private array $methods;
+
+    private ?string $name;
+
+    /**
+     * Set of middleware FQCNs.
+     *
+     * @var string[]
+     */
+    private array $middleware;
+
+    /**
+     * @param string|string[] $method
+     * @param string|string[] $middleware
+     */
+    public function __construct(
+        string $pattern,
+        string | array $method = [RequestMethod::GET],
+        string $name = null,
+        string | array $middleware = []
+    ) {
+        $this->pattern    = $pattern;
+        $this->methods    = is_string($method) === true ? [$method] : $method;
+        $this->name       = $name;
+        $this->middleware = is_string($middleware) === true ? [$middleware] : $middleware;
+    }
+
+    public function getPattern(): string
+    {
+        return $this->pattern;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getMethods(): array
+    {
+        return $this->methods;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getMiddleware(): array
+    {
+        return $this->middleware;
+    }
+}
---
+++ tests/Api/RouteTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jerowork\RouteAttributeProvider\Test\Api;
+
+use Jerowork\RouteAttributeProvider\Api\RequestMethod;
+use Jerowork\RouteAttributeProvider\Api\Route;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+final class RouteTest extends TestCase
+{
+    public function testItShouldConstructMinimalistRoute(): void
+    {
+        $route = new Route('/minimalist');
+
+        $this->assertSame('/minimalist', $route->getPattern());
+        $this->assertSame([RequestMethod::GET], $route->getMethods());
+        $this->assertNull($route->getName());
+        $this->assertSame([], $route->getMiddleware());
+    }
+
+    public function testItShouldConstructExtendedRoute(): void
+    {
+        $route = new Route('/extended', RequestMethod::PUT, 'extended.route', stdClass::class);
+
+        $this->assertSame('/extended', $route->getPattern());
+        $this->assertSame([RequestMethod::PUT], $route->getMethods());
+        $this->assertSame('extended.route', $route->getName());
+        $this->assertSame([stdClass::class], $route->getMiddleware());
+    }
+
+    public function testItShouldConstructFullFledgedRoute(): void
+    {
+        $route = new Route(
+            '/full-fledged',
+            [RequestMethod::GET, RequestMethod::POST],
+            'full-fledged.route',
+            [stdClass::class, stdClass::class]
+        );
+
+        $this->assertSame('/full-fledged', $route->getPattern());
+        $this->assertSame([RequestMethod::GET, RequestMethod::POST], $route->getMethods());
+        $this->assertSame('full-fledged.route', $route->getName());
+        $this->assertSame([stdClass::class, stdClass::class], $route->getMiddleware());
+    }
+}
