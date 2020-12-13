34e790869ed0e31166d809f2390a76a3dc0ed1f9
Jeroen de Graaf <hello@jero.work>
2020-12-13 15:07:08 +0100

    Setup mappers to map LoadedRoute[] to valid cache payload format

---
+++ src/RouteLoader/Cache/MapCachePayloadToLoadedRoutesTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jerowork\RouteAttributeProvider\RouteLoader\Cache;
+
+use Generator;
+use Jerowork\RouteAttributeProvider\RouteLoader\LoadedRoute;
+use JsonException;
+
+trait MapCachePayloadToLoadedRoutesTrait
+{
+    /**
+     * @return Generator<LoadedRoute>
+     *
+     * @throws JsonException
+     */
+    public function mapCachePayloadToLoadedRoutes(mixed $payload): Generator
+    {
+        yield from array_map(
+            static fn (array $payload): LoadedRoute => LoadedRoute::fromPayload($payload),
+            json_decode($payload, true, flags: JSON_THROW_ON_ERROR)
+        );
+    }
+}
